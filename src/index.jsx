ff3a299bb6f4baac35e1277c7f40b07cbe792b82
MuhammadMoiz200099 <muhammadmoiz0087@gmail.com>
2024-04-09 05:12:21 +0500

    updates

--- src/index.jsx
+++ src/index.jsx
@@ -3,11 +3,30 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+      <ToastContainer
+        position="bottom-left"
+        theme="dark"
+        autoClose={3500}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        draggable
+      />
+    </Provider>
   </React.StrictMode>
 );
 
